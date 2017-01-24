ef2a7632b86f3a349655a30bde3bfdc1616912ff
Agarwal <mahima.agarwal@accenture.com>
2017-01-24 11:55:05 +0530

    removed ms dependencies

--- src/main/java/com/accenture/microservices/emp/timerecords/web/AttendanceController.java
+++ src/main/java/com/accenture/microservices/emp/timerecords/web/AttendanceController.java
@@ -20,11 +20,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.accenture.microservices.emp.business.AttendanceCalculator;
 import com.accenture.microservices.emp.data.EmployeeAttendance;
-import com.accenture.microservices.emp.timerecords.client.ChargeCodeService;
-import com.accenture.microservices.emp.timerecords.client.vo.ChargeCode;
-import com.accenture.microservices.emp.timerecords.client.vo.EmployeeAssignments;
-import com.accenture.microservices.emp.timerecords.client.vo.EmployeeDetails;
-import com.accenture.microservices.emp.timerecords.service.TimeRecordsService;
+//import com.accenture.microservices.emp.timerecords.client.ChargeCodeService;
+//import com.accenture.microservices.emp.timerecords.client.vo.ChargeCode;
+//import com.accenture.microservices.emp.timerecords.client.vo.EmployeeAssignments;
+//import com.accenture.microservices.emp.timerecords.client.vo.EmployeeDetails;
+//import com.accenture.microservices.emp.timerecords.service.TimeRecordsService;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -54,8 +54,8 @@ public class AttendanceController {
 	@Autowired
 	DataOperationsController dataOperationController;
 	
-	@Autowired
-	TimeRecordsService timeRecordsService;
+//	@Autowired
+//	TimeRecordsService timeRecordsService;
 	
 	@RequestMapping(value = "/timerecords/{empId}", method=RequestMethod.GET)
 	public Collection<EmployeeAttendance> getEmployeeAttendance(@PathVariable("empId") Integer id){
@@ -99,58 +99,58 @@ public class AttendanceController {
 		return null;
 	}
 	
-	@RequestMapping(value = "/timerecords/chargecodes/{wbs}", method = RequestMethod.GET)
-	public ChargeCode getChargeCodeDetails(@PathVariable("wbs") String wbs){
-		
-		log.info("request received: "+"/timerecords/chargecodes/"+wbs);
-		ChargeCode chargeCode=timeRecordsService.getChargeCodeDetails(wbs);
-		
-		return	chargeCode;
-	}
+//	@RequestMapping(value = "/timerecords/chargecodes/{wbs}", method = RequestMethod.GET)
+//	public ChargeCode getChargeCodeDetails(@PathVariable("wbs") String wbs){
+//		
+//		log.info("request received: "+"/timerecords/chargecodes/"+wbs);
+//		ChargeCode chargeCode=timeRecordsService.getChargeCodeDetails(wbs);
+//		
+//		return	chargeCode;
+//	}
+//	
+//	@RequestMapping(value = "/timerecords/chargecodes/{wbs}/employees/{empid}", method = RequestMethod.GET)
+//	public ChargeCode getChargeCodeDetailsOfAnEmployee(@PathVariable("wbs") String wbs,@PathVariable("empid") Integer empid){
+//		
+//		log.info("request received: "+"/timerecords/chargecodes/"+wbs+"/employees/"+empid);		
+//		ChargeCode chargeCode=timeRecordsService.getChargeCodeDetailsOfAnEmployee(wbs, empid);
+//		
+//		return	chargeCode;
+//	}
+//	
+//	@RequestMapping(value = "/timerecords/employees/assignments/{empId}", method=RequestMethod.GET)
+//	public EmployeeAssignments getEmployeeAssignment(@PathVariable("empId") Integer empId){
+//		
+//		log.info("request received: "+"/timerecords/employees/assignments/"+empId);
+//		
+//		EmployeeAssignments employeeAssignments=null;
+//		try{
+//			employeeAssignments=timeRecordsService.getEmployeeAssignment(empId);
+//		}catch(Exception e){
+//			e.printStackTrace();
+//		}
+//		
+//		return employeeAssignments;
+//		
+//	}
 	
-	@RequestMapping(value = "/timerecords/chargecodes/{wbs}/employees/{empid}", method = RequestMethod.GET)
-	public ChargeCode getChargeCodeDetailsOfAnEmployee(@PathVariable("wbs") String wbs,@PathVariable("empid") Integer empid){
-		
-		log.info("request received: "+"/timerecords/chargecodes/"+wbs+"/employees/"+empid);		
-		ChargeCode chargeCode=timeRecordsService.getChargeCodeDetailsOfAnEmployee(wbs, empid);
-		
-		return	chargeCode;
-	}
 	
-	@RequestMapping(value = "/timerecords/employees/assignments/{empId}", method=RequestMethod.GET)
-	public EmployeeAssignments getEmployeeAssignment(@PathVariable("empId") Integer empId){
-		
-		log.info("request received: "+"/timerecords/employees/assignments/"+empId);
-		
-		EmployeeAssignments employeeAssignments=null;
-		try{
-			employeeAssignments=timeRecordsService.getEmployeeAssignment(empId);
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-		
-		return employeeAssignments;
-		
-	}
-	
-	
-	@RequestMapping(value = "/timerecords/employees/{empId}", method=RequestMethod.GET)
-	public EmployeeDetails getEmployeeDetail(@PathVariable("empId") Integer empId){
-		
-		log.info("request received: "+"/timerecords/employees/{empId}"+empId);
-		
-		EmployeeDetails employeeDetail=null;
-		try{
-			employeeDetail=timeRecordsService.getEmployeeDetails(empId);
-		}catch(Exception e){
-			log.info("Error in get getEmployeeDetail " +e);
-		}
-		
-		return employeeDetail;
-		
-	}
-	
-	@RequestMapping(value = "/timerecords/employees", method=RequestMethod.GET)
+//	@RequestMapping(value = "/timerecords/employees/{empId}", method=RequestMethod.GET)
+//	public EmployeeDetails getEmployeeDetail(@PathVariable("empId") Integer empId){
+//		
+//		log.info("request received: "+"/timerecords/employees/{empId}"+empId);
+//		
+//		EmployeeDetails employeeDetail=null;
+//		try{
+//			employeeDetail=timeRecordsService.getEmployeeDetails(empId);
+//		}catch(Exception e){
+//			log.info("Error in get getEmployeeDetail " +e);
+//		}
+//		
+//		return employeeDetail;
+//		
+//	}
+//	
+/*	@RequestMapping(value = "/timerecords/employees", method=RequestMethod.GET)
 	public  List<EmployeeDetails> getAllEmployees(){
 		
 		log.info("request received: "+"/timerecords/employees");
@@ -166,6 +166,6 @@ public class AttendanceController {
 		
 		return EmployeeDetailsList;
 		
-	}
+	}*/
 
 }
